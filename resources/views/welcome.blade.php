0eef27d058ecce6074665c9560dc3e5b98a9072b
ZainJon06 <zainjob.74@devaims.com>
2022-09-30 19:10:36 +0500

    initial update

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+@extends('layout')
+@section('content')
+    <div class="container">
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">NAME</th>
+                <th scope="col">GMAIL</th>
+                <th scope="col">PHONE</th>
+                <th scope="col">ACTION</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>1</th>
+                <th>1</th>
+                <th>1</th>
+                <th>1</th>
+                <th>1</th>
+              </tr>
+            </tbody>
+          </table>
+
+    </div>
+@endsection()
