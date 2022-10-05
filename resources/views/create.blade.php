05ce8e2706386784e530014cb9ef5b0311d97a4a
ZainJon06 <zainjob.74@devaims.com>
2022-10-05 18:53:30 +0500

    fsdg

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="container">
 
-    <form method="post" action="submit">
+    <form method="post" action="submit" enctype="multipart/form-data">
         @csrf
         <div class="form-group">
             <label for="exampleInputEmail1">Name</label>
@@ -16,7 +16,10 @@
             <label for="exampleInputPassword1">Phone</label>
             <input type="phone" name="phone" class="form-control" id="exampleInputPassword1" placeholder="Enter Phone Number">
         </div>
-
+        <div class="form-group">
+            <label for="exampleInputPassword1">Image</label>
+            <input type="file" name="img" class="form-control" id="exampleInputPassword1">
+        </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 </div>
